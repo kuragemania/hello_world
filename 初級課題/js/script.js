57373820ad24a64963ab69ffa5a6c3606bff0d49
kuragemania <115987573+kuragemania@users.noreply.github.com>
2023-04-27 12:08:56 +0900

    Add files via upload

---
+++ 初級課題/js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer, .js-drawer a").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  effect: "fade",
+  speed: 3000,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+
