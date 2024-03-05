ece866e35e2e9838126ad2cb075469c66f87770f
kuragemania <115987573+kuragemania@users.noreply.github.com>
2024-03-05 11:47:09 +0900

    Add files via upload

---
+++ ademo/pf1/js/script.js
@@ -0,0 +1,35 @@
+jQuery(function ($) {
+  // ハンバーガーメニュー
+  $(function () {
+      $(".js-hamburger").on("click", function () {
+          $(this).toggleClass("is-open");
+          if ($(this).hasClass("is-open")) {
+              openDrawer();
+          } else {
+              closeDrawer();
+          }
+      });
+
+      // backgroundまたはページ内リンクをクリックで閉じる
+      $(".js-drawer a[href]").on("click", function () {
+          closeDrawer();
+      });
+
+      // resizeイベント
+      $(window).on('resize', function() {
+          if (window.matchMedia("(min-width: 768px)").matches) {
+              closeDrawer();
+          }
+      });
+  });
+
+  function openDrawer() {
+      $(".js-drawer").fadeIn();
+      $(".js-hamburger").addClass("is-open");
+  }
+
+  function closeDrawer() {
+          $(".js-drawer").fadeOut();
+      $(".js-hamburger").removeClass("is-open");
+  }
+});
